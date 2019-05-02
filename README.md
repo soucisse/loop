d982344af428b728a9c8b7b4b9d4953105fb8494
soucisse <bousquet.etienne@courrier.uqam.ca>
2019-05-02 19:27:03 -0400

    Premier commit du projet loop

---
+++ README.md
@@ -0,0 +1,7 @@
+# Programme LOOP
+
+Ce programme est une expérimentation de la structure de contrôle *for*.
+
+## Compte
+
+Le programme compte de 1 à 9 et indique la différence du chiffre affiché avec 10. 
---
+++ loop.c
@@ -0,0 +1,21 @@
+//loop.c
+//Programme pour expérimenter avec les structures de contrôle
+
+#include <stdio.h>
+
+int main (int argc, char *argv[]) {
+
+	printf("Bonjour\nVoici un test des structures de contrôle\n");
+
+	int i;
+
+	for (i = 0; i < 10; i++){
+		printf("Le chiffre %d vaut %d de moins que 10\n\n", i, 10-i);
+	}
+
+	printf("Le programme est terminé\n");
+
+	return 0;  
+
+}
+
